14be2484b6b356bab6c229e34845c1acda2077ae
Alexandre Aittouares <alex.aittouares@gmail.com>
2026-05-03 03:06:12 +0200

    Add check for existing User email during user creation

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersMongoRepository } from './users.mongo.repository';
@@ -9,10 +9,20 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersMongoRepository) {}
 
   async create(createUserDto: CreateUserDto) {
+    await this.checkForExistingUser(createUserDto);
     return this.usersRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
     });
+  }
+
+  private async checkForExistingUser(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+    } catch (err) {
+      return;
+    }
+    throw new UnprocessableEntityException('Email already exists.');
   }
 
   async getUser(getUserDto: GetUserDto) {
