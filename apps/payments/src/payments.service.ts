10d851ab1ddda4a4720eb6177d97615f26c482b9
Alexandre Aittouares <alex.aittouares@gmail.com>
2026-05-04 02:22:24 +0200

    Connect Reservations microservice to Payments microservice Modify createcharge into payments microservice to handle stripe API changes

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CreateChargeDto } from '@app/common';
 import { Stripe as StripeType } from 'node_modules/stripe/cjs/stripe.core';
 import Stripe from 'stripe';
-import { CreateChargeDto } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -12,18 +12,20 @@ export class PaymentsService {
     this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY')!);
   }
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
+  async createCharge({ /*card,*/ amount }: CreateChargeDto) {
+    /*const paymentMethod = await this.stripe.paymentMethods.create({
       type: 'card',
       card,
-    });
+    });*/
 
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
+      // payment_method: paymentMethod.id,
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
-      currency: 'usd',
+      //payment_method_types: ['card'],
+      payment_method: 'pm_card_mastercard',
+      currency: 'eur',
+      automatic_payment_methods: { enabled: true, allow_redirects: 'never' },
     });
 
     return paymentIntent;
